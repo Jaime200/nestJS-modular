d70d92407bf729cd5ed7faae5fac327a0ec03042
OneSolutionsGT <38265654+onesolutionsGT@users.noreply.github.com>
2021-05-12 16:22:35 -0600

    se realiza la parametrizacionde inyeccion en providers, se utiliza useFactory, y se utiliza el modulo de HttpService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule, HttpService } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -9,12 +9,20 @@ const API_KEY = '1234WSDFE44'
 const API_KEY_PROD = '1234_PROD'
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [UsersModule, ProductsModule, HttpModule],
   controllers: [AppController],
   providers: [AppService, 
     {
       provide : 'API_KEY',
-      useValue: process.env.NODE_ENV ==='prod' ? API_KEY_PROD : API_KEY_PROD
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY
+    },
+    {
+      provide : 'task',
+      useFactory: async (http: HttpService) =>{
+        const task = await http.get('https://jsonplaceholder.typicode.com/todos').toPromise();
+        return task.data;
+       },
+      inject: [HttpService]
     }
   ],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,10 +2,13 @@ import { Injectable, Inject } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  constructor( @Inject('API_KEY') private apiKey:string){
+  constructor( @Inject('API_KEY') private apiKey:string, 
+    @Inject('task') private task: any,
+  ){
     
   }
   getHello(): string {
+    console.log(this.task);
     return 'Hello World! API' + this.apiKey;
   }
 }
