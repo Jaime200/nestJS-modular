7b6a0be33e6277e30ae9e182a806f576b7ce3df7
OneSolutionsGT <38265654+onesolutionsGT@users.noreply.github.com>
2021-05-12 15:57:18 -0600

    se realiza la parametrizacionde inyeccion en providers, se utiliza useValue

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
+
+const API_KEY = '1234WSDFE44'
+const API_KEY_PROD = '1234_PROD'
+
 @Module({
   imports: [UsersModule, ProductsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, 
+    {
+      provide : 'API_KEY',
+      useValue: process.env.NODE_ENV ==='prod' ? API_KEY_PROD : API_KEY_PROD
+    }
+  ],
 })
 export class AppModule {}
