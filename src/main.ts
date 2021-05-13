1dba6b0739630ec98c95bb1addf69cbb2e88e3ce
Jaime Muñoz <jaime.munoz200@gmail.com>
2021-05-12 22:49:19 -0600

    Parametrizacion de documentacion automatica

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
@@ -10,6 +10,17 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+
+  const config = new DocumentBuilder()
+  .setTitle('API')
+  .setDescription('Platzi store')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+
   await app.listen(3000);
 }
 bootstrap();
