bb8a5f5f04687ab906f59b938d5d4090869c9e12
Jaime Muñoz <jaime.munoz200@gmail.com>
2021-05-12 22:38:35 -0600

    Se agrega tipado, se crea config.ts, se agrega registerAs, se injecta con Inject

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,22 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config'
+import { ConfigType } from '@nestjs/config'
+import config from './config'
 @Injectable()
 export class AppService {
   constructor( 
     @Inject('API_KEY') private apiKey:string, 
     @Inject('task') private task: any,
-    private configservice:ConfigService
+    @Inject(config.KEY) private configservice: ConfigType<typeof config>
+    //private configservice:ConfigService
+    
   ){
     
   }
   getHello(): string {
-    const apiKey = this.configservice.get('API_KEY');
-    const dbName = this.configservice.get('DATABASE_NAME');
+    //const apiKey = this.configservice.get('API_KEY');
+    const apiKey = this.configservice.apiKey
+    //const dbName = this.configservice.get('DATABASE_NAME');
+    const dbName = this.configservice.database.name;
     return 'Hello World! API ' + apiKey + ' db ' + dbName  ;
   }
 }
